34996b83a82f7a7a2dc2e70c1ed28d58aa321aa8
PanSirko765 <ipienkin@pl5.ukr.education>
2025-07-16 18:31:21 +0300

    Some updates
    future door animation added(beta)

--- Assets/I_S/OpenDoor_Script.cs
+++ Assets/I_S/OpenDoor_Script.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,17 +10,24 @@ public class OpenDoor_Script : MonoBehaviour
 
     [SerializeField] Animation animation1;
     [SerializeField] Animation animation2;
+    [SerializeField] GameObject g_o;
+    [SerializeField] GameObject card;
+    [SerializeField] Material material1;
+    public bool f1 = true;
     void Update()
     {
-        animation1.GetComponent<Animation>().Play();    
+        
     }
-    public void OnTriggerStay(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player") )
+        if (other.gameObject.CompareTag("Player") && f1==true)
         {
-
+            card.SetActive(true);  
             animation1.Play();
             animation2.Play();
+            g_o.GetComponent<MeshRenderer>().material = material1;   
+            
+            f1 =false;   
         }
     }
     
