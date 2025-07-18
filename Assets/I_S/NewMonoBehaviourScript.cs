8d8e8b48c3297531c8536e59a2b1675299c48351
PanSirko765 <ipienkin@pl5.ukr.education>
2025-07-18 18:39:49 +0300

    added 2-nd door
    /

--- Assets/I_S/NewMonoBehaviourScript.cs
+++ Assets/I_S/NewMonoBehaviourScript.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] Animation animation1;
+    [SerializeField] Animation animation2;
+    [SerializeField] GameObject Key;
+   
+    public bool f1 = true;
+    public bool f2 = false;
+    public bool f3 = true;
+    public float Timer = 0f;
     void Update()
     {
-        
+        if (f2 == true)
+        {
+            if (Timer < 3 && f3 == true)
+            {
+                Timer += 0.043f;
+                if (Timer >= 3)
+                {
+                    f3 = false;
+                    Key.SetActive(false);
+                   
+                }
+            }
+
+        }
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && f1 == true)
+        {
+            Key.SetActive(true);
+            animation1.Play();
+            animation2.Play();
+
+
+            f1 = false;
+            f2 = true;
+
+        }
     }
 }
